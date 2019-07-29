b8052411856a62fbbbc5e5d51706dc92cc358503
Tian du Toit <tian@hmsnz.com>
2019-07-29 14:31:13 +1200

    Add slice

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	fmt.Println(card)
+	cards := []string{newCard(), newCard(), "Ace of Diamonds"}
+	cards = append(cards, "Six of Spades")
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
