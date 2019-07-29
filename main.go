ee194f4593c99e2bde1b4947cae4cc06b8ec2c98
Tian du Toit <tian@hmsnz.com>
2019-07-29 15:20:39 +1200

    Add deck file and newDeck function

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{newCard(), newCard(), "Ace of Diamonds"}
-	cards = append(cards, "Six of Spades")
-	fmt.Println(cards)
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.print()
+	// fmt.Println(cards)
 }
