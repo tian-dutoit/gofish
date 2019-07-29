ca8c4774be59922143f449db4d58e0e2179db2de
Tian du Toit <tian@hmsnz.com>
2019-07-29 15:54:41 +1200

    Add WriteFile function

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -24,12 +25,17 @@ func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
-func (d deck) toString() string {
-	return strings.Join([]string(d), ",")
-}
-
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+
 }
