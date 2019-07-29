30f71172e4aaacbcae8a06f4abdaa8d668807ac6
Tian du Toit <tian@hmsnz.com>
2019-07-29 14:13:38 +1200

    Change to implicit

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// create a new variable and assign str then print out
-	var card string = "Ace of Spades"
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
 	fmt.Println(card)
 }
