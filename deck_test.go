e79eaa24182008b7401e46168eba201dd847f89c
Tian du Toit <tian@hmsnz.com>
2019-07-29 16:39:23 +1200

    Add test

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,10 @@
 package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	cards := newDeck()
+	if len(cards) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(cards))
+	}
+}
